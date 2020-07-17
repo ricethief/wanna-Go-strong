10f962b9967843bd602204738397059002ed19d1
Se Chang Park <ricetheifaus@gmail.com>
2020-07-17 19:39:24 +0930

    Wip

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"wanna-Go-strong/model"
+)
+
+func main() {
+	e := model.NewExercise("Squat", "")
+	fmt.Println(e)
+}
---
+++ model/exercise.go
@@ -0,0 +1,41 @@
+package model
+
+import (
+	"fmt"
+)
+
+//Exercise struct
+type Exercise struct {
+	Name      string    //name of the exercise
+	VideoLink string    //url for videolink
+	Sets      []float64 //set of Exercise[reps][intensity]
+}
+
+//constructor
+func NewExercise(name, videoLink string) Exercise {
+
+	var SetsArray = []float64{}
+	//this constructor assign SetArray as a Sets[][]float64
+	e := Exercise{Name: name, VideoLink: videoLink, Sets: SetsArray}
+	return e
+}
+
+//addSet adds new rap,intensity into the Exercise
+func (e *Exercise) addSet(_rep float64, _intensity float64) {
+	rep := _rep             //rep array
+	intensity := _intensity //intensity array
+	e.Sets = append(e.Sets, rep)
+	e.Sets = append(e.Sets, intensity)
+
+	fmt.Println("set added", e.Sets)
+}
+
+//deleteing a set from the behind
+//return deleted array do nothing if array is empty
+func (e *Exercise) deleteSet() {
+	if len(e.Sets) == 0 {
+		fmt.Println("Nothing to Delete here :)")
+	} else {
+		e.Sets = e.Sets[:len(e.Sets)-2]
+	}
+}
