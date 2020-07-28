d87b83e6e6e9961a63bbc3e63b8359d481b9ac17
Se Chang Park <ricetheifaus@gmail.com>
2020-07-28 14:41:40 +0930

    playing with templates

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"html/template"
 	"log"
@@ -11,7 +10,29 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func getVolumNFrequency(w http.ResponseWriter, r *http.Request) {
+//compile templates on start
+var templates = template.Must(template.ParseFiles("templates/header.html", "templates/footer.html", "templates/about.html", "templates/main.html"))
+
+//A Page structure
+type Page struct {
+	Title string
+}
+
+//Display the named template
+func display(w http.ResponseWriter, tmpl string, data interface{}) {
+	templates.ExecuteTemplate(w, tmpl, data)
+}
+
+//The handlers.
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	display(w, "main", &Page{Title: "Home"})
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	display(w, "about", &Page{Title: "About"})
+}
+
+func getvnfHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>We Have Volume as jason</h1> \n")
 	vars := mux.Vars(r)
 	gender := vars["gender"]
@@ -28,7 +49,6 @@ func getVolumNFrequency(w http.ResponseWriter, r *http.Request) {
 
 	input := []string{gender, wt, ht, strength, exp, age, diet,
 		sleep, stress, hw, hra}
-	json.NewEncoder(w).Encode(model.NewVolumeNFrequency(input)) //passing json
 
 	parsedTemplate, _ := template.ParseFiles("templates/index.html")
 	err := parsedTemplate.Execute(w, model.NewVolumeNFrequency(input))
@@ -38,17 +58,12 @@ func getVolumNFrequency(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func testPostArticles(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Testing Post endpoint")
-}
-
 func handleRequest() {
-	myRouter := mux.NewRouter().StrictSlash(true)
-
-	myRouter.HandleFunc("/getVNF/{gender}/{wt}/{ht}/{strength}/{exp}/{age}/{diet}/{sleep}/{stress}/{hw}/{hra}", getVolumNFrequency)
+	myRouter := mux.NewRouter().StrictSlash(true) //declare mux router
+	myRouter.HandleFunc("/", mainHandler)
+	myRouter.HandleFunc("/about", aboutHandler)
+	myRouter.HandleFunc("/getVNF/{gender}/{wt}/{ht}/{strength}/{exp}/{age}/{diet}/{sleep}/{stress}/{hw}/{hra}", getvnfHandler)
 	myRouter.PathPrefix("/").Handler(http.StripPrefix("/static", http.FileServer(http.Dir("static/"))))
-	myRouter.HandleFunc("/getvnf/{hra}", getVolumNFrequency)
-	myRouter.HandleFunc("/articles", testPostArticles).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8082", myRouter))
 
 }
